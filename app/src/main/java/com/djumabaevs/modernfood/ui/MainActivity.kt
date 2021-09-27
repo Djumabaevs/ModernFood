50af655b9386b821540f7fca6574de2d2c34114e
Djumabaevs <djumabaevb@gmail.com>
2021-09-28 00:32:33 +0600

    little refactor to follow clean architecture

--- app/src/main/java/com/djumabaevs/modernfood/ui/MainActivity.kt
+++ app/src/main/java/com/djumabaevs/modernfood/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.djumabaevs.modernfood
+package com.djumabaevs.modernfood.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,6 +7,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.djumabaevs.modernfood.R
 import com.djumabaevs.modernfood.databinding.ActivityMainBinding
 
 
