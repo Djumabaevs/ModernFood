7ef65a13ee23f3251ed9ae5e3a23b37fd8a4f8d5
Djumabaevs <djumabaevb@gmail.com>
2021-09-28 00:34:30 +0600

    add retrofit

---
+++ app/src/main/java/com/djumabaevs/modernfood/Constants.kt
@@ -0,0 +1,12 @@
+package com.djumabaevs.modernfood
+
+class Constants {
+
+    companion object {
+
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val API_KEY = "bb87494a78704fdb8cc30d6275ee7acc"
+
+    }
+
+}
