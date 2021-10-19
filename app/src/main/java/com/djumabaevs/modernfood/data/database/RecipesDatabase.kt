5d4209f0ab925820d431e9bff714dc3c1d8663d5
Djumabaevs <djumabaevb@gmail.com>
2021-10-19 09:32:55 +0600

    recipes database

---
+++ app/src/main/java/com/djumabaevs/modernfood/data/database/RecipesDatabase.kt
@@ -0,0 +1,17 @@
+package com.djumabaevs.modernfood.data.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+@Database(
+    entities = [RecipesEntity::class, FavoritesEntity::class, FoodJokeEntity::class],
+    version = 1,
+    exportSchema = false
+)
+@TypeConverters(RecipesTypeConverter::class)
+abstract class RecipesDatabase: RoomDatabase() {
+
+    abstract fun recipesDao(): RecipesDao
+
+}
